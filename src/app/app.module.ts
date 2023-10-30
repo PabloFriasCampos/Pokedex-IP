f35d2e5c0acfad8e32533c9bc5bf6e9a1bb084ce
PabloFriasCampos <pfricam2505@g.educaand.es>
2023-10-30 17:59:42 +0100

    Icono y creacion de componente lista y buscador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonComponent } from './model/pokemon/pokemon.component';
+import { ListaComponent } from './lista/lista.component';
+import { BuscadorComponent } from './buscador/buscador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonComponent
+    PokemonComponent,
+    ListaComponent,
+    BuscadorComponent
   ],
   imports: [
     BrowserModule,
