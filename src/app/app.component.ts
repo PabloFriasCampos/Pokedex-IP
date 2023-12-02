69165d185f6fc78cf538a23d059e90ab4326452c
PabloFriasCampos <p4blule@gmail.com>
2023-12-02 19:43:30 +0100

    bug primera vez sin elegir language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Pokedex-IP';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    // Verificar si la clave "language" existe en el localStorage
+    const language = localStorage.getItem('language');
+
+    // Si no existe, establecerla en "en"
+    if (!language) {
+      localStorage.setItem('language', 'en');
+    }
+
+  }
+
 }
